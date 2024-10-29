0fb681476b91a6c33105b0bd6b450aae09bcc3b7
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 23:22:36 +0100

    Delete restart.sh

--- restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-
-cd ~/scripts/$folder
-./start-node.sh
-sleep 30s
-./start-farmer.sh
