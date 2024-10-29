c0fbe68ef21a1e51446b387e608ef987a35ae5a0
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 12:45:44 +0100

    Update stop-farmer.sh

--- stop-farmer.sh
+++ stop-farmer.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ -z $1 ]
-  then 
-    echo "Running farmers:"
-    ps aux | grep subspace-farmer | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/subspace//g'
-    echo "------------------------"
-    read -p "Farmer?  " id
-    echo "------------------------"
-  else 
-    id=$1
-fi
-
-process=$(ps aux | grep subspace-farmer | grep -v grep | grep "subspace$id" | awk '{print $2}')
+process=$(ps aux | grep subspace-farmer | grep -v grep | grep $folder | awk '{print $2}')
 echo "Killing process $process..."
 kill $process
