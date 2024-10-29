658237a28d8a4e19b305ed668d289db5d01720dc
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 22:23:50 +0100

    Update stop-node.sh

--- stop-node.sh
+++ stop-node.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-
-process=$(ps aux | grep subspace-node | grep -v grep | grep "$folder" | awk '{print $2}')
-echo "Killing process $process..."
-kill $process
+pkill subspace-node
