24d367ba1a265c42c3b7196be5d98bbf8b271102
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 23:17:35 +0100

    Update start-farmer.sh

--- start-farmer.sh
+++ start-farmer.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-source /root/scripts/$folder/config
-farmer=$(ls $EXEC | grep subspace-farmer)
-
-echo "Starting farmer $folder ($farmer $RPC $REWARD $DISKS)"
-cd $EXEC
-
-#./$farmer farm --node-rpc-url $rpc --reward-address $reward path=$base,size=$size &>> ~/logs/subspace_farmer$id &
-
-./$farmer farm --reward-address $REWARD $DISKS &>>~/logs/$folder.farmer &
-tail -f ~/logs/$folder.farmer
+systemctl restart autonomys-farmer.service
+journalctl -n 200 -u autonomys-farmer.service -f --no-hostname -o cat
