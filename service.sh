69434108fd526d8e89614abd5d48624ffedf317e
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 23:16:00 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -19,3 +19,20 @@ RestartSec=30
 [Install]
 WantedBy=multi-user.target
 EOF
+
+sudo tee /etc/systemd/system/autonomys-farmer.service > /dev/null <<EOF
+[Unit]
+Description=autonomys-farmer
+After=network.target
+[Service]
+User=root
+ExecStart=$EXEC/$farmer farm --reward-address $REWARD $DISKS
+Restart=always
+RestartSec=30
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable autonomys-node
+sudo systemctl enable autonomys-farmer
