d632e656363907560f3ef921456bdba3849e6142
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 23:18:36 +0100

    Update log-farmer.sh

--- log-farmer.sh
+++ log-farmer.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-
-tail -f ~/logs/$folder.farmer
+journalctl -n 200 -u autonomys-node.service -f --no-hostname -o cat
