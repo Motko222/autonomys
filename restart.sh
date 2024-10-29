c69e323aed2e7b4ba20da68635c10d146daa195d
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 12:47:38 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
 
-if [ -z $1 ]
-  then 
-    echo "Running nodes:"
-    ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/subspace//g'
-    echo "------------------------"
-    read -p "Node?  " id
-    echo "------------------------"
-  else 
-    id=$1
-  fi
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
 
-source ~/scripts/subspace/config/node$id
-
-fpid=$(ps aux | grep -w $base | grep subspace-farmer-ubuntu | awk '{print $2}')
-if [ ! -z $fpid ]
-  then 
-    echo "Stopping farmer process "$fpid
-    kill $fpid 
-  else 
-    echo "Farmer not running"
-fi
-
-npid=$(ps aux | grep -w $base | grep subspace-node-ubuntu | awk '{print $2}')
-if [ ! -z $npid ]
-  then 
-    echo "Stopping node process "$npid
-    kill $npid
-    sleep 5s
-  else 
-    echo "Node not running"
-fi
-
-cd ~/scripts/subspace
-./start-node.sh $id
-sleep 10s
-./start-farmer.sh $id
+cd ~/scripts/$folder
+./start-node.sh
+sleep 30s
+./start-farmer.sh
