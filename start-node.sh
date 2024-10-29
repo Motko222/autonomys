4e09dc5b4ea080d1f7f7052424317707935e65b6
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 23:16:54 +0100

    Update start-node.sh

--- start-node.sh
+++ start-node.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-source ~/scripts/$folder/config
-node=$(ls $EXEC | grep subspace-node)
-
-echo "Starting node $folder ($BASE $node $CHAIN $NAME $PEERS)"
-cd $EXEC
-
-#./$node run --chain $chain --base-path $base --farmer --listen-on /ip4/0.0.0.0/tcp/$port --rpc-listen-on 127.0.0.1:$wsport \
-#    --in-peers $peers --out-peers $peers --name $name &> ~/logs/subspace_node$id &
-
-./$node run --chain $CHAIN --base-path $BASE --name $NAME --farmer &>/root/logs/$folder.node &
-tail -f ~/logs/$folder.node
+systemctl restart autonomys-node.service
