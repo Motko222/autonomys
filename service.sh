ecc684bc20e9ba985ceb717c6e2b36d1b1126e7b
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-29 23:10:12 +0100

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+source /root/scripts/$folder/config
+
+node=$(ls $EXEC | grep subspace-node)
+farmer=$(ls $EXEC | grep subspace-farmer)
+
+sudo tee /etc/systemd/system/autonomys-node.service > /dev/null <<EOF
+[Unit]
+Description=autonomys-node
+After=network.target
+[Service]
+User=root
+ExecStart=$EXEC/$node run --chain $CHAIN --base-path $BASE --name $NAME --farmer
+Restart=always
+RestartSec=30
+[Install]
+WantedBy=multi-user.target
+EOF
