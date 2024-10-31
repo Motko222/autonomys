bed7ab602099c04ea4e19cb797ee37ea8d181002
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-31 21:31:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,17 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 source ~/scripts/$folder/config
 
 read -p "Sure? " c
-case $c in
-  y|Y)
-    [ ! -d $EXEC ] && mkdir $EXEC
-    cd $EXEC
-    rm subspace-node* subspace-farmer*
-    echo "Get links from here: https://docs.subspace.network/docs/protocol/substrate-cli"
-    read -p "Node URL? " url
-    wget $url
-    read -p "Farmer URL? " url
-    wget $url
-    node=$(ls | grep subspace-node)
-    farmer=$(ls | grep subspace-farmer)
-    chmod 777 $node
-    chmod 777 $farmer
-  ;;
-esac
+case $c in y|Y) ;; *) exit ;; esac
+
+[ ! -d $EXEC ] && mkdir $EXEC
+cd $EXEC
+rm subspace-node* subspace-farmer*
+echo "Get links from here: https://docs.subspace.network/docs/protocol/substrate-cli"
+read -p "Node URL? " url
+wget $url
+read -p "Farmer URL? " url
+wget $url
+node=$(ls | grep subspace-node)
+farmer=$(ls | grep subspace-farmer)
+chmod 777 $node
+chmod 777 $farmer
